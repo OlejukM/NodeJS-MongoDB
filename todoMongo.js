51946766b96f4d8d9cef45b47c65b5aaca2d3e01
OlejukM <olejukmaslo@gmail.com>
2019-01-26 16:42:41 +0200

    unnecessary file was deleted

--- todoMongo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require( 'mongoose' );
-const Schema = mongoose.Schema;
-
-const todoSchema = new Schema({
-    task: String,
-    done: Boolean,
-});
-
-module.exports = mongoose.model('Todo', todoSchema);
